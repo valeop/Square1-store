0586f04b67a00e6608dc39d4e009aa626a0bec5c
Valeria OP <valeria.ortiz2001@gmail.com>
2025-01-28 00:03:31 -0500

    product sections erased from Homepage

--- scripts/createProductCard.js
+++ scripts/createProductCard.js
@@ -1,3 +1,5 @@
+const newArrivalsContent = document.querySelector(".new-arrivals-bottom");
+
 const product = {
     name: "Camiseta Oversized",
     brand: "Gucci",
@@ -24,11 +26,9 @@ const colorButton = (color) => {
     return button;
 };
 
-const newArrivalsContent = document.querySelector(".new-arrivals-bottom");
-const productCard = document.createElement("article");
-
-productCard.classList.add("product-card");
-productCard.innerHTML = `
+const createProductCard = (product) => {
+    const productCard = document.createElement("article"); productCard.classList.add("product-card");
+    productCard.innerHTML = `
 <a href="#">
     <img class="product-card_image" src="${product.image.src}" alt="${product.image.alt}">
 </a>
@@ -38,17 +38,61 @@ productCard.innerHTML = `
 <span class="product-card_brand">${product.brand}</span>
 <div class="product-card_prices">
     <!-- Discount price -->
-    <span class="product-card_discount-price">${product.price.discount}</span>
+    <span class="product-card_discount-price">${product.price.discount === 0
+            ? product.price.original
+            : product.price.discount}</span>
     <!-- Original price -->
     <span class="product-card_original-price">${product.price.original}</span>
 </div>
 <div class="product-card_colors"></div>
-<button class="product-card_buy">Comprar</button>
-`;
+<button class="product-card_buy">Comprar</button>`;
 
-const productColorBox = productCard.querySelector(".product-card_colors");
-product.colors.forEach((color) => {
-    productColorBox.appendChild(colorButton(color));
-});
+    const productColorBox = productCard.querySelector(".product-card_colors");
+    product.colors.forEach((color) => {
+        productColorBox.appendChild(colorButton(color));
+    });
 
-newArrivalsContent.appendChild(productCard);
+    newArrivalsContent.appendChild(productCard);
+};
+
+newArrivalsContent.innerHTML = "";
+
+const getProducts = async () => {
+    return fetch("https://fakestoreapi.com/products").then((response) => response.json());
+};
+
+const mountProducts = async () => {
+    const products = await getProducts();
+
+    products.forEach((product) => {
+        const tempProduct = {
+            name: product.title,
+            brand: product.category,
+            image: {
+                src: product.image,
+                alt: product.title
+            },
+            price: {
+                original: product.price,
+                discount: product.price * 0
+            },
+            colors: [
+                [222, 284, 135],
+                [33, 111, 204],
+                [45, 87, 44]
+            ]
+        }
+        createProductCard(tempProduct);
+    });
+};
+
+mountProducts();
+
+// createProductCard(product);
+// createProductCard(product);
+// createProductCard(product);
+// createProductCard(product);
+// createProductCard(product);
+// createProductCard(product);
+// createProductCard(product);
+// createProductCard(product);
