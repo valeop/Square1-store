b957f484c076ebb59b4abb85102974f7cb13b2e3
Valeria OP <valeria.ortiz2001@gmail.com>
2025-02-06 17:37:33 -0500

    minicart, home, shop, productPage and cartPage ready

--- scripts/productPage.js
+++ scripts/productPage.js
@@ -1,8 +1,14 @@
 import "./header.js";
 
 document.getElementById("close-mini-cart").addEventListener("click", closeMiniCart, false);
+document.getElementById("minicart-confirm-btn").addEventListener("click", openCartPage, false);
 
 function closeMiniCart() {
     const closeBtn = document.querySelector(".overlay");
     closeBtn.setAttribute("style", "display: none");
 }
+
+function openCartPage() {
+    window.location.href = "cartPage.html";
+}
+
